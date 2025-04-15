aa81479b73fc11aa35a66efdcf7a78dd4b4253a6
Myles Conti <myles@contibros.com>
2025-04-15 10:33:57 +1000

    Initial Commit

---
+++ Xe-Image Convert/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Xe-Image Convert
+//
+//  Created by STUDIO ONE on 15/4/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Xe-Image Convert/Xe_Image_ConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xe_Image_ConvertApp.swift
+//  Xe-Image Convert
+//
+//  Created by STUDIO ONE on 15/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct Xe_Image_ConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
